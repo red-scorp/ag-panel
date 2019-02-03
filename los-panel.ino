592dc0fbf5d1671dd3504a6ee16b5b58735564d0
Andriy Golovnya <andriy.golovnya@goolemail.com>
2019-02-03 13:31:11 +0100

    Made code look nicer.

--- los-panel.ino
+++ los-panel.ino
@@ -6,35 +6,46 @@
 
 #define FW_VERSION        __TIME__ " " __DATE__
 
-// these constants won't change.  But you can change the size of
-// your LCD using them:
-#define LCD_ROWS          2
-#define LCD_COLS          16
+#define LCD_PIN_RS          12
+#define LCD_PIN_RW          11
+#define LCD_PIN_E           10
+#define LCD_PIN_BACKLIGHT   9
 
-#define LOS_INSTRUCTION   0xFE
-#define LOS_KEYPAD        0xFE
-#define LOS_BACKLIGHT     0xFD
+#define LCD_PIN_D4          5
+#define LCD_PIN_D5          4
+#define LCD_PIN_D6          3
+#define LCD_PIN_D7          2
 
-#define LOS_BACKLIGHT_OFF 0x00
-#define LOS_BACKLIGHT_ON  0xFF
+#define LCD_ROWS            2
+#define LCD_COLS            16
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
-// RS, RW, Enable, 4,5,6,7 (Refer to the back of your LCD for details)
+#define LOS_INSTRUCTION     0xFE
+#define LOS_KEYPAD          0xFE
+#define LOS_BACKLIGHT       0xFD
+
+#define LOS_BACKLIGHT_INIT  0x20
+#define LOS_BACKLIGHT_OFF   0x00
+#define LOS_BACKLIGHT_ON    0xFF
+
+#define LOG_UART_BOD        9600
+
+LiquidCrystal lcd(LCD_PIN_RS, LCD_PIN_RW, LCD_PIN_E, LCD_PIN_D4, LCD_PIN_D5, LCD_PIN_D6, LCD_PIN_D7);
 
 void setup() { 
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(9, OUTPUT);
-  Serial.begin(9600);
-  // set up the LCD's number of rows and columns: 
+  pinMode(LCD_PIN_BACKLIGHT, OUTPUT);
+  Serial.begin(LOG_UART_BOD);
+
   digitalWrite(LED_BUILTIN, HIGH);
-  analogWrite(9, 25);
+  analogWrite(LCD_PIN_BACKLIGHT, LOS_BACKLIGHT_INIT);
+
   lcd.begin(LCD_ROWS, LCD_COLS);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("LoS-Panel Ready...");
   lcd.setCursor(0, 1);
   lcd.print(FW_VERSION);
+
   digitalWrite(LED_BUILTIN, LOW);
 }
 
@@ -44,7 +55,6 @@ byte serial_getch() {
 //    delay(10);
   }
 
-  // read the incoming byte:
   return Serial.read();
 }
 
@@ -52,7 +62,7 @@ void loop() {
 
   byte rxbyte;
 
-  rxbyte = serial_getch(); // Fetch byte
+  rxbyte = serial_getch();
 
   switch(rxbyte) {
 
@@ -62,11 +72,13 @@ void loop() {
 
   case LOS_BACKLIGHT:
     digitalWrite(LED_BUILTIN, HIGH);
-    analogWrite(9, serial_getch());
+    analogWrite(LCD_PIN_BACKLIGHT, serial_getch());
     break;
 
   default:
     lcd.write(rxbyte);
     break;
   }
+
+  // TODO: add check of keyboard pins and send data to pc
 }
