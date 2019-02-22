7fa15ab334051fa44b9381f208715bbb822e4c03
Andriy Golovnya <andriy.golovnya@googemail.com>
2019-02-22 09:28:14 +0100

    Made config.h more readable.

--- alos-panel/config.h
+++ alos-panel/config.h
@@ -8,19 +8,23 @@
 #define CONFIG_H
 
 /* FirmWare Configuration */
+/* ------------------------------------------------------------------------- */
 #define FW_NAME             "ALoS" /* Arduino LCD over Serial Panel */
 #define FW_VERSION          __TIME__ " " __DATE__
 
 /* UART Configuration */
+/* ------------------------------------------------------------------------- */
 #define UART_DIRECT
 //#define UART_BUFFERED
 
 #define UART_BAUD           9600
 
-/* for UART_BUFFERED */
+#ifdef UART_BUFFERED
 #define UART_BUF_SIZE       512
+#endif
 
 /* LCD Configuration */
+/* ------------------------------------------------------------------------- */
 #define LCD_4BIT
 //#define LCD_8BIT
 //#define LCD_I2C
@@ -32,35 +36,42 @@
 #define LCD_ROWS            2
 #define LCD_COLS            16
 
-/* for LCD_4BIT and LCD_8BIT */
+#if defined(LCD_4BIT) || defined(LCD_8BIT)
 #define LCD_PIN_RS          8
 #define LCD_PIN_RW          255
 #define LCD_PIN_ENABLE      9
 #define LCD_PIN_BACKLIGHT   10
-//#define LCD_PIN_D0          255
-//#define LCD_PIN_D1          255
-//#define LCD_PIN_D2          255
-//#define LCD_PIN_D3          255
+#ifdef LCD_8BIT
+#define LCD_PIN_D0          255
+#define LCD_PIN_D1          255
+#define LCD_PIN_D2          255
+#define LCD_PIN_D3          255
+#endif
 #define LCD_PIN_D4          4
 #define LCD_PIN_D5          5
 #define LCD_PIN_D6          6
 #define LCD_PIN_D7          7
+#endif
 
-/* for LCD_I2C */
-//#define LCD_I2C_ADDR        0x27
+#ifdef LCD_I2C
+#define LCD_I2C_ADDR        0x27
+#endif
 
+#ifdef LCD_BACKLIGHT_PWM
 #define LCD_BL_PWM_INIT     0x20
+#endif
 
 /* Keyboard Configuration */
+/* ------------------------------------------------------------------------- */
 //#define KBD_NONE
 //#define KBD_D_MATRIX
 //#define KBD_A_JOYSTICK
 #define KBD_A_KEYPAD
 
+#ifdef KBD_D_MATRIX
 #define KBD_ROWS            4
 #define KBD_COLS            4
 
-/* for KBD_D_MATRIX */
 #define KBD_PIN_C1          13
 #define KBD_PIN_C2          8
 #define KBD_PIN_C3          7
@@ -69,13 +80,15 @@
 #define KBD_PIN_R2          A1
 #define KBD_PIN_R3          A2
 #define KBD_PIN_R4          A3
+#endif
 
-/* for KBD_A_JOYSTICK */
+#ifdef KBD_A_JOYSTICK
 #define KBD_PIN_X           A0
 #define KBD_PIN_Y           A1
 #define KBD_PIN_BTN         13
+#endif
 
-/* for KBD_A_KEYPAD */
+#ifdef KBD_A_KEYPAD
 #define KBD_PIN_DATA        A0
 #define KBD_DATA_NONE_MIN   700
 #define KBD_DATA_NONE_MAX   1023
@@ -89,5 +102,6 @@
 #define KBD_DATA_RIGHT_MAX  50
 #define KBD_DATA_SELECT_MIN 550
 #define KBD_DATA_SELECT_MAX 700
+#endif
 
 #endif//CONFIG_H
