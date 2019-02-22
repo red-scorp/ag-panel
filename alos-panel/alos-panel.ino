c69944d2c8e7ef4bed8b86fc226e961892c9a3bc
Andriy Golovnya <andriy.golovnya@goolemail.com>
2019-02-22 18:54:35 +0100

    Remove key repetitions. Add some comments.

--- alos-panel/alos-panel.ino
+++ alos-panel/alos-panel.ino
@@ -11,6 +11,11 @@
 #include "lcd.h"
 #include "kbd.h"
 
+/*! \brief Main initialisation function
+ * - setup UART
+ * - setup LCD
+ * - setup keyboard
+ */
 void setup() { 
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -20,10 +25,16 @@ void setup() {
 
   lcd_init();
   lcd_welcome();
+  kbd_init();
 
   digitalWrite(LED_BUILTIN, LOW);
 }
 
+/*! \brief Main loop function
+ * - read UART
+ * - parse UART data and transfer it to LCD
+ * - start background task
+ */
 void loop() {
 
   uint8_t rxbyte = uart_getch();
@@ -46,9 +57,20 @@ void loop() {
   los_yield();
 }
 
+/*! \brief Background task function
+ * - read keyboard
+ * - remove key repetitions
+ * - send key over UART
+ */
 void los_yield() {
 
+  static uint8_t old_key = KBD_KEY_NONE;
   uint8_t key = kbd_getkey();
+
+  if(old_key != key)
+    old_key = key;
+  else
+    return;
 
   if(key != KBD_KEY_NONE) {
     uart_putch(LOS_KEYPAD);
