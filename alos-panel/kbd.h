3240170cc323237c8cd86a4752f089c4f48f975e
Andriy Golovnya <andriy.golovnya@goolemail.com>
2019-02-22 23:50:21 +0100

    Fixing common key codes.

--- alos-panel/kbd.h
+++ alos-panel/kbd.h
@@ -9,13 +9,13 @@
 
 #include "config.h"
 
-#define KBD_KEY_NONE        0xFF
+#define KBD_KEY_NONE        0x00
 
-#define KBD_KEY_UP          0x01
-#define KBD_KEY_DOWN        0x02
-#define KBD_KEY_LEFT        0x03
-#define KBD_KEY_RIGHT       0x04
-#define KBD_KEY_SELECT      0x05
+#define KBD_KEY_UP          0x01  /* KeyMatrix_1_1=Up */
+#define KBD_KEY_DOWN        0x02  /* KeyMatrix_1_2=Down */
+#define KBD_KEY_LEFT        0x04  /* KeyMatrix_1_3=Left */
+#define KBD_KEY_RIGHT       0x08  /* KeyMatrix_1_4=Right */
+#define KBD_KEY_SELECT      0x11  /* KeyMatrix_2_1=Enter */
 
 void kbd_init();
 uint8_t kbd_getkey();
