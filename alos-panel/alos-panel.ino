b54e0413351a41b569c33acbb728ae6c2344b9ff
Andriy Golovnya <andriy.golovnya@goolemail.com>
2019-06-11 14:19:01 +0200

    Main code reorganization

--- alos-panel/alos-panel.ino
+++ alos-panel/alos-panel.ino
@@ -1,80 +1,10 @@
 /*!
-  \file uart.cpp
-  \brief ALoS-Panel Project main code
+  \file alos-panel.ino
+  \brief ALoS-Panel Project file
   \author Andriy Golovnya (andriy.golovnya@googlemail.com)
 */
 
-#include "src/private.h"
-#include "config.h"
-#include "src/los.h"
-#include "src/uart/uart.h"
-#include "src/lcd/lcd.h"
-#include "src/kbd/kbd.h"
-
-/*! \brief Main initialization function
- * - setup UART
- * - setup LCD
- * - setup keyboard
- */
-void setup() { 
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  uart_init();
-  lcd_init();
-  lcd_welcome();
-  kbd_init();
-
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
-/*! \brief Main loop function
- * - read UART
- * - parse UART data and transfer it to LCD
- * - start background task
- */
-void loop() {
-
-  uint8_t rxbyte = uart_getch();
-
-  switch(rxbyte) {
-
-  case LOS_INSTRUCTION:
-    lcd_command(uart_getch());
-    break;
-
-  case LOS_BACKLIGHT:
-    lcd_set_backlight(uart_getch());
-    break;
-
-  default:
-    lcd_write(rxbyte);
-    break;
-  }
-
-  los_yield();
-}
-
-/*! \brief Background task function
- * - read keyboard
- * - remove key repetitions
- * - send key over UART
- */
-void los_yield() {
-
-  static uint8_t old_key = KBD_KEY_NONE;
-  uint8_t key = kbd_getkey();
-
-  if(old_key != key)
-    old_key = key;
-  else
-    return;
-
-  if(key != KBD_KEY_NONE) {
-    uart_putch(LOS_KEYPAD);
-    uart_putch(key);
-  }
-
-  uart_prefill();
-}
+/*
+  Please open config.h to configure your ALoS panel!
+  Refere to https://github.com/red-scorp/alos-panel for more information.
+*/
---
+++ alos-panel/src/los.cpp
@@ -0,0 +1,80 @@
+/*!
+  \file los.cpp
+  \brief ALoS-Panel Project main code
+  \author Andriy Golovnya (andriy.golovnya@googlemail.com)
+*/
+
+#include "private.h"
+#include "../config.h"
+#include "los.h"
+#include "uart/uart.h"
+#include "lcd/lcd.h"
+#include "kbd/kbd.h"
+
+/*! \brief Main initialization function
+ * - setup UART
+ * - setup LCD
+ * - setup keyboard
+ */
+void setup() { 
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  uart_init();
+  lcd_init();
+  lcd_welcome();
+  kbd_init();
+
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+/*! \brief Main loop function
+ * - read UART
+ * - parse UART data and transfer it to LCD
+ * - start background task
+ */
+void loop() {
+
+  uint8_t rxbyte = uart_getch();
+
+  switch(rxbyte) {
+
+  case LOS_INSTRUCTION:
+    lcd_command(uart_getch());
+    break;
+
+  case LOS_BACKLIGHT:
+    lcd_set_backlight(uart_getch());
+    break;
+
+  default:
+    lcd_write(rxbyte);
+    break;
+  }
+
+  los_yield();
+}
+
+/*! \brief Background task function
+ * - read keyboard
+ * - remove key repetitions
+ * - send key over UART
+ */
+void los_yield() {
+
+  static uint8_t old_key = KBD_KEY_NONE;
+  uint8_t key = kbd_getkey();
+
+  if(old_key != key)
+    old_key = key;
+  else
+    return;
+
+  if(key != KBD_KEY_NONE) {
+    uart_putch(LOS_KEYPAD);
+    uart_putch(key);
+  }
+
+  uart_prefill();
+}
