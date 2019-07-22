15bd61ed7d905e8c47e102d2c38c1bd319bff707
Andriy Golovnya <andriy.golovnya@googemail.com>
2019-07-22 17:54:20 +0200

    cleanup on new lcd function prototypes

--- ag-panel/src/lcd/lcd.h
+++ ag-panel/src/lcd/lcd.h
@@ -28,14 +28,14 @@ void lcd_print_string(const char *str);
 
 /* New LCD functions for Hitachi HD44780 LCD emulation */
 void lcd_clear();
-void lcd_home();
+//void lcd_home(); -> lcd_set_cursor
 void lcd_set_cursor(uint8_t x, uint8_t y);
-void lcd_display(uint8_t on);
-void lcd_cursor(uint8_t on);
-void lcd_blink(uint8_t on);
-void lcd_scroll(uint8_t right);
-void lcd_direction(uint8_t left2right);
-void lcd_autoscroll(uint8_t on);
-void lcd_create_char(uint8_t pos, uint8_t *buf);
+//void lcd_display(uint8_t on); -> part of protocol emulator
+//void lcd_cursor(uint8_t on); -> not supported
+//void lcd_blink(uint8_t on); -> not supported
+//void lcd_scroll(uint8_t right); -> part of protocol emulator
+//void lcd_direction(uint8_t left2right); -> part of protocol emulator
+//void lcd_autoscroll(uint8_t on); -> part of protocol emulator
+void lcd_create_char(uint8_t pos, uint8_t *buf); // to be defined!
 
 #endif//LCD_H
