1e1cee02e45aecbfde1d805a67924b09c8aefacf
Andriy Golovnya <andriy.golovnya@goolemail.com>
2019-10-29 21:55:38 +0100

    Fix compile time warning about name collision

--- ag-panel/src/lcd/lcd_text_i2c_rgb.cpp
+++ ag-panel/src/lcd/lcd_text_i2c_rgb.cpp
@@ -17,57 +17,57 @@
 
 #include <Adafruit_RGBLCDShield.h>
 
-Adafruit_RGBLCDShield lcd;
+Adafruit_RGBLCDShield rgb_lcd;
 
 /*! \brief Initialization of LCD display itself
  */
 void lcd_init_display() {
-  lcd.begin(LCD_ROWS, LCD_COLS);
+  rgb_lcd.begin(LCD_ROWS, LCD_COLS);
 }
 
 /*! \brief Initialization of LCD display backlight LED
  */
 void lcd_init_backlight() {
-  lcd.setBacklight(LCD_BACKLIGHT_COLOR);
+  rgb_lcd.setBacklight(LCD_BACKLIGHT_COLOR);
 }
 
 /*! \brief Set brightness of LCD display backlight LED
  */
 void lcd_set_backlight(uint8_t brightness) {
   if(brightness == 0)
-    lcd.setBacklight(0); /* does it work? */
+    rgb_lcd.setBacklight(0); /* does it work? */
   else
-    lcd.setBacklight(LCD_BACKLIGHT_COLOR);
+    rgb_lcd.setBacklight(LCD_BACKLIGHT_COLOR);
 }
 
 /*! \brief Write character to LCD display directly
  */
 void lcd_write_display(uint8_t txbyte) {
-  lcd.write(txbyte);
+  rgb_lcd.write(txbyte);
 }
 
 /*! \brief Send command to LCD display directly
  */
 void lcd_command_display(uint8_t txbyte) {
-  lcd.command(txbyte);
+  rgb_lcd.command(txbyte);
 }
 
 /*! \brief Clear LCD display
  */
 void lcd_clear_display() {
-  lcd.clear();
+  rgb_lcd.clear();
 }
 
 /*! \brief Set cursor to specific positon on LCD display
  */
 void lcd_set_cursor(uint8_t row, uint8_t col) {
-  lcd.setCursor(row, col);
+  rgb_lcd.setCursor(row, col);
 }
 
 /*! \brief Print a string on LCD display
  */
 void lcd_print_string(const char *str) {
-  lcd.print(str);
+  rgb_lcd.print(str);
 }
 
 #endif//LCD_TEXT_I2C_RGB
