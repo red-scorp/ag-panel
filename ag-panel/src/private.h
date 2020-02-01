2af8a596f7c4cf749162df232f24fa20f5e46a76
Andriy Golovnya <andriy.golovnya@goolemail.com>
2020-02-01 09:52:16 +0100

    Add few other platforms to adc resolution definition.

--- ag-panel/src/private.h
+++ ag-panel/src/private.h
@@ -22,7 +22,7 @@ const int MaxPWMValue = (1 << PWMResolution) - 1;
 
 #if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_ESP8266)
   const uint8_t AnalogResolution = 10;
-#elif defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_ESP32)
+#elif defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_STM32) || defined(ARDUINO_ARCH_ESP32)
   const uint8_t AnalogResolution = 12;
 #else
   #warning The value of 'AnalogResolution' is not defined for this platform yet! Assuming 10-bit!
