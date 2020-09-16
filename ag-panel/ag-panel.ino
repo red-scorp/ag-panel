f6ed9fd0768e242e055cbb2ad629c1877de0bdc2
Andriy Golovnya <andriy.golovnya@googlemail.com>
2020-09-16 16:44:38 +0200

    add some human readable hints

--- ag-panel/ag-panel.ino
+++ ag-panel/ag-panel.ino
@@ -1,11 +1,14 @@
 /*!
   \file ag-panel.ino
   \brief AG-Panel Project file
-  \copyright (C) 2019 Andriy Golovnya
+  \copyright (C) 2019-2020 Andriy Golovnya
   \author Andriy Golovnya (andriy.golovnya@googlemail.com)
 */
 
 /*
   Please open config.h to configure your AG-Panel!
   Refere to https://github.com/red-scorp/ag-panel for more information.
+
+  This file is intentionally left blank.
+  The project code can be found in src directory.
 */
