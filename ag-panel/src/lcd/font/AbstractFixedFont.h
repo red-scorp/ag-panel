6bbc0283361745c2bbff40acd1c71e1ea3d506a2
Andriy Golovnya <andriy.golovnya@goolemail.com>
2020-11-03 21:15:18 +0100

    Add default glyph variable.

--- ag-panel/src/lcd/font/AbstractFixedFont.h
+++ ag-panel/src/lcd/font/AbstractFixedFont.h
@@ -24,15 +24,16 @@ public:
     m_GlyphYSize(0),
     m_GlyphData(nullptr),
     m_DataRangeBegin(0),
-    m_DataRangeEnd(0) { Init(); }
+    m_DataRangeEnd(0),
+    m_DefaultGlyph(0) { Init(); }
   virtual ~AbstractFixedFont() { Exit(); }
 
   /*! Get normal X size of font glyphs in pixels */
-  virtual uint8_t GetGlyphXSize(void) const { return m_GlyphXSize; };
+  virtual uint8_t GetGlyphXSize(void) const override { return m_GlyphXSize; };
   /*! Get normal Y size of font glyphs in pixels */
-  virtual uint8_t GetGlyphYSize(void) const { return m_GlyphYSize; };
+  virtual uint8_t GetGlyphYSize(void) const override { return m_GlyphYSize; };
 
-  virtual const Glyph *GetGlyph(uint8_t number) { /* TODO: do a magic here! */ };
+  virtual const Glyph *GetGlyph(uint8_t number) override { /* TODO: do a magic here! */ };
 
 protected:
   uint8_t m_GlyphXSize;       /*!< Size of a glyph in X direction */
@@ -40,6 +41,7 @@ protected:
   uint8_t *m_GlyphData;       /*!< Pointer to an arry of glyph raw data */
   uint16_t m_DataRangeBegin;  /*!< Number of first glyph in data array */
   uint16_t m_DataRangeEnd;    /*!< Number of last glyph in data array */
+  uint16_t m_DefaultGlyph;    /*!< Default 'missing' glyph number */
 
 private:
   bool Init() { return true; }
