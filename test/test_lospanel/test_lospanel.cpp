e0d5c9ec61739baca1f730e205e4ba0189c91c66
Andriy Golovnya <andriy.golovnya@gmail.com>
2023-09-14 10:42:38 +0200

    Added printing to LCD unit test

--- test/test_lospanel/test_lospanel.cpp
+++ test/test_lospanel/test_lospanel.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
 /* Trick to include unit test headers during build and have syntax highlighting in editor */
 #if defined(UNIT_TEST)
@@ -30,8 +31,8 @@
 #define MOCK_UART_RXBUF_SIZE        512
 #define MOCK_LCD_WIDTH              20
 #define MOCK_LCD_HEIGHT             2
-#define MOCK_LCD_WRITE_BUF_SIZE     64
-#define MOCK_LCD_COMMAND_BUF_SIZE   64
+#define MOCK_LCD_WRITE_BUF_SIZE     128
+#define MOCK_LCD_COMMAND_BUF_SIZE   128
 
 using namespace std;
 
@@ -52,10 +53,10 @@ MOCK_VARIABLE int i_MockTextLCD_Print_str_called = 0;
 MOCK_VARIABLE int i_MockTextLCD_Print_ch_called = 0;
 MOCK_VARIABLE int i_MockTextLCD_Write_called = 0;
 MOCK_VARIABLE int i_MockTextLCD_Command_called = 0;
-MOCK_VARIABLE uint8_t i_MockTextLCD_WriteBuffer[MOCK_LCD_WRITE_BUF_SIZE] = { 0 };
-MOCK_VARIABLE uint16_t i_MockTextLCD_WriteBufferPos = 0;
-MOCK_VARIABLE uint8_t i_MockTextLCD_CommandBuffer[MOCK_LCD_COMMAND_BUF_SIZE] = { 0 };
-MOCK_VARIABLE uint16_t i_MockTextLCD_CommandBufferPos = 0;
+MOCK_VARIABLE uint8_t c_MockTextLCD_WriteBuffer[MOCK_LCD_WRITE_BUF_SIZE] = { 0 };
+MOCK_VARIABLE int i_MockTextLCD_WriteBufferPos = 0;
+MOCK_VARIABLE uint8_t c_MockTextLCD_CommandBuffer[MOCK_LCD_COMMAND_BUF_SIZE] = { 0 };
+MOCK_VARIABLE int i_MockTextLCD_CommandBufferPos = 0;
 class MockTextLCD: public AbstractTextLCD {
 public:
     MockTextLCD(AbstractBacklight *Backlight): AbstractTextLCD(Backlight, MOCK_LCD_WIDTH, MOCK_LCD_HEIGHT) { }
@@ -64,23 +65,47 @@ public:
     virtual void SetCursor(uint8_t x, uint8_t y) override { i_MockTextLCD_SetCursor_called++; }
     virtual void Print(const char *str) override { i_MockTextLCD_Print_str_called++; }
     virtual void Print(char ch) override { i_MockTextLCD_Print_ch_called++; }
-    virtual void Write(uint8_t byte) override { i_MockTextLCD_Write_called++; }
-    virtual void Command(uint8_t byte) override { i_MockTextLCD_Command_called++; }
+    virtual void Write(uint8_t byte) override {
+        i_MockTextLCD_Write_called++;
+        if(i_MockTextLCD_WriteBufferPos < MOCK_LCD_WRITE_BUF_SIZE)
+            c_MockTextLCD_WriteBuffer[i_MockTextLCD_WriteBufferPos++] = byte;
+     }
+    virtual void Command(uint8_t byte) override {
+        i_MockTextLCD_Command_called++;
+        if(i_MockTextLCD_CommandBufferPos < MOCK_LCD_COMMAND_BUF_SIZE)
+            c_MockTextLCD_CommandBuffer[i_MockTextLCD_CommandBufferPos++] = byte;
+    }
 };
 MOCK_VARIABLE int i_MockUART_GetBaudRate_called = 0;
 MOCK_VARIABLE int i_MockUART_PutCh_called = 0;
 MOCK_VARIABLE int i_MockUART_GetCh_called = 0;
-MOCK_VARIABLE uint8_t i_MockUART_TxBuffer[MOCK_UART_TXBUF_SIZE] = { 0 };
-MOCK_VARIABLE uint16_t i_MockUART_TxBufferPos = 0;
-MOCK_VARIABLE uint8_t i_MockUART_RxBuffer[MOCK_UART_RXBUF_SIZE] = { 0 };
-MOCK_VARIABLE uint16_t i_MockUART_RxBufferPos = 0;
+MOCK_VARIABLE int i_MockUART_Available_called = 0;
+MOCK_VARIABLE uint8_t c_MockUART_TxBuffer[MOCK_UART_TXBUF_SIZE] = { 0 };
+MOCK_VARIABLE int i_MockUART_TxBufferPos = 0;
+MOCK_VARIABLE uint8_t c_MockUART_RxBuffer[MOCK_UART_RXBUF_SIZE] = { 0 };
+MOCK_VARIABLE int i_MockUART_RxBufferPos = 0;
+MOCK_VARIABLE int i_MockUART_RxBufferMax = 0;
 class MockUART: public AbstractUART {
 public:
     MockUART(): AbstractUART() { }
     virtual ~MockUART() { }
-    virtual uint8_t PutCh(uint8_t TxByte) override { i_MockUART_GetCh_called++; return 0; }
-    virtual uint8_t GetCh() override { i_MockUART_PutCh_called++; return 0; }
+    virtual uint8_t PutCh(uint8_t TxByte) override {
+        i_MockUART_PutCh_called++;
+        if(i_MockUART_TxBufferPos < MOCK_UART_TXBUF_SIZE)
+            c_MockUART_TxBuffer[i_MockUART_TxBufferPos++] = TxByte;
+        return 0;
+    }
+    virtual uint8_t GetCh() override {
+        i_MockUART_GetCh_called++;
+        if(i_MockUART_RxBufferPos < i_MockUART_RxBufferMax && i_MockUART_RxBufferPos < MOCK_UART_RXBUF_SIZE)
+            return c_MockUART_RxBuffer[i_MockUART_RxBufferPos++];
+        return 0;
+    }
     virtual uint32_t GetBaudRate() const override { i_MockUART_GetBaudRate_called++; return AbstractUART::GetBaudRate(); }
+    virtual uint32_t Available() override {
+        i_MockUART_Available_called++;
+        return i_MockUART_RxBufferMax - i_MockUART_RxBufferPos;
+    }
 };
 MOCK_VARIABLE int i_MockKeyboard_GetKey_called = 0;
 MOCK_VARIABLE int i_MockKeyboard_GetKeyCount_called = 0;
@@ -92,7 +117,12 @@ public:
     virtual uint8_t GetKeyCount() override { i_MockKeyboard_GetKeyCount_called++; return 0; }
 };
 MOCK_VARIABLE int i_micros_called = 0;
-MOCK_FUNCTION unsigned long micros() { i_micros_called++; return 0; }
+MOCK_FUNCTION unsigned long micros() {
+    i_micros_called++;
+    struct timeval ts;
+    gettimeofday(&ts, NULL);
+    return ts.tv_sec * 1000000 + ts.tv_usec;
+}
 
 #undef DEBUG_STR
 #define DEBUG_STR(str) printf("%s", str)
@@ -111,17 +141,19 @@ void setUp(void) {
     i_MockTextLCD_Print_ch_called = 0;
     i_MockTextLCD_Write_called = 0;
     i_MockTextLCD_Command_called = 0;
-    memset(i_MockTextLCD_WriteBuffer, 0, sizeof(i_MockTextLCD_WriteBuffer));
+    memset(c_MockTextLCD_WriteBuffer, 0, sizeof(c_MockTextLCD_WriteBuffer));
     i_MockTextLCD_WriteBufferPos = 0;
-    memset(i_MockTextLCD_CommandBuffer, 0, sizeof(i_MockTextLCD_CommandBuffer));
+    memset(c_MockTextLCD_CommandBuffer, 0, sizeof(c_MockTextLCD_CommandBuffer));
     i_MockTextLCD_CommandBufferPos = 0;
     i_MockUART_GetBaudRate_called = 0;
     i_MockUART_PutCh_called = 0;
     i_MockUART_GetCh_called = 0;
-    memset(i_MockUART_TxBuffer, 0, sizeof(i_MockUART_TxBuffer));
+    i_MockUART_Available_called = 0;
+    memset(c_MockUART_TxBuffer, 0, sizeof(c_MockUART_TxBuffer));
     i_MockUART_TxBufferPos = 0;
-    memset(i_MockUART_RxBuffer, 0, sizeof(i_MockUART_RxBuffer));
+    memset(c_MockUART_RxBuffer, 0, sizeof(c_MockUART_RxBuffer));
     i_MockUART_RxBufferPos = 0;
+    i_MockUART_RxBufferMax = 0;
     i_MockKeyboard_GetKey_called = 0;
     i_MockKeyboard_GetKeyCount_called = 0;
     i_micros_called = 0;
@@ -131,7 +163,36 @@ void setUp(void) {
 void tearDown(void) {
 }
 
+/* Unit test function to check if initialization of mocks for LoSPanelProtocol class works correctly */
 void test_lospanel_protocol_does_right_initialization(void) {
+
+    AbstractUART *p_UART = new MockUART();
+    TEST_ASSERT_NOT_NULL(p_UART);
+
+    AbstractBacklight *p_Backlight = new MockBacklight();
+    TEST_ASSERT_NOT_NULL(p_Backlight);
+
+    AbstractTextLCD *p_TextLCD = new MockTextLCD(p_Backlight);
+    TEST_ASSERT_NOT_NULL(p_TextLCD);
+
+    AbstractKeyboard *p_Keyboard = new MockKeyboard();
+    TEST_ASSERT_NOT_NULL(p_Keyboard);
+
+    AbstractProtocol *p_Protocol = new LoSPanelProtocol(p_UART, p_TextLCD, p_Keyboard);
+    TEST_ASSERT_NOT_NULL(p_Protocol);
+
+    delete p_Protocol;
+    delete p_Keyboard;
+    delete p_TextLCD;
+    delete p_Backlight;
+    delete p_UART;
+}
+
+/* Unit test function to check if LoSPanelProtocol class read data from UART and sends to LCD */
+void test_lospanel_protocol_reads_uart_and_prints_on_lcd(void) {
+
+    uint8_t c_UARTInputData[] = { 'A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5' };
+    uint8_t c_LCDOutputData[] = { 'A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5' };
 
     AbstractUART *p_UART = new MockUART();
     AbstractBacklight *p_Backlight = new MockBacklight();
@@ -139,7 +200,21 @@ void test_lospanel_protocol_does_right_initialization(void) {
     AbstractKeyboard *p_Keyboard = new MockKeyboard();
     AbstractProtocol *p_Protocol = new LoSPanelProtocol(p_UART, p_TextLCD, p_Keyboard);
 
-    // TODO: test initialization of LoSPanelProtocol class
+    memcpy(c_MockUART_RxBuffer, c_UARTInputData, sizeof(c_UARTInputData));
+    i_MockUART_RxBufferPos = 0;
+    i_MockUART_RxBufferMax = sizeof(c_UARTInputData);
+
+    /* Calling the function under test till it runs out of input data */
+    while(i_MockUART_RxBufferPos < i_MockUART_RxBufferMax)
+        p_Protocol->Loop();
+
+    /* Checking if the function under test called right functions */
+    TEST_ASSERT_EQUAL_INT(sizeof(c_UARTInputData), i_MockUART_GetCh_called);
+    TEST_ASSERT_EQUAL_INT(sizeof(c_LCDOutputData), i_MockTextLCD_Write_called);
+
+    /* Checking if the function under test does right output */
+    TEST_ASSERT_EQUAL_INT(sizeof(c_LCDOutputData), i_MockTextLCD_WriteBufferPos);
+    TEST_ASSERT_EQUAL_INT(0, memcmp(c_LCDOutputData, c_MockTextLCD_WriteBuffer, sizeof(c_LCDOutputData)));
 
     delete p_Protocol;
     delete p_Keyboard;
@@ -156,6 +231,7 @@ int main(int argc, char *argv[]) {
 
     /* Calling unit test functions */
     RUN_TEST(test_lospanel_protocol_does_right_initialization);
+    RUN_TEST(test_lospanel_protocol_reads_uart_and_prints_on_lcd);
 
     UNITY_END();
 }
